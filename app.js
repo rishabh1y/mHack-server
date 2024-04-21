0224696217ee8d7d2985622bf02a979af4913a3e
Rishabh Yadav <rishabhyadav082@gmail.com>
2024-04-21 11:41:30 +0530

    Trying to resolve error

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 import passport from "passport";
 import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import cors from "cors";
+import { MemoryStore } from "express-session";
+
 
 const app = express();
 export default app;
@@ -23,10 +25,15 @@ app.use(
     saveUninitialized:false,
 
     cookie:{
+        maxAge: 86400000,
         secure: process.env.NODE_ENV === "development" ? false : true,
         httpOnly: process.env.NODE_ENV === "development" ? false : true,
         sameSite: process.env.NODE_ENV === "development" ? false : "none",
     },
+
+    store: new MemoryStore({
+        checkPeriod: 86400000 // prune expired entries every 24h
+      }),
 })
 );
 
